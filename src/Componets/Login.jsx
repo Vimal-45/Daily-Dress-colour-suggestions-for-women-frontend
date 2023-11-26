d3fb54a56b11788f8ea5261687b99c31f6366164
Vimal-45 <dvimal45@gmail.com>
2023-11-26 22:51:27 +0530

    change text alignment

--- src/Componets/Login.jsx
+++ src/Componets/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img style={{height:120}}
+                <img style={{ height: 120 }}
                     className="mx-auto  w-auto"
                     // src="https://i.pinimg.com/originals/cb/ac/d4/cbacd4816617e88cc7b9528d841b6084.png"
                     src={Logo}
@@ -67,7 +67,7 @@ const Login = () => {
                                 onChange={(e) => setEmail(e.target.value)}
                                 value={email}
                                 className="block w-full rounded-md py-1.5 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6 text-center"
-                                />
+                            />
                         </div>
                     </div>
 
@@ -87,7 +87,7 @@ const Login = () => {
                                 onChange={(e) => setPassword(e.target.value)}
                                 value={password}
                                 className="block w-full rounded-md py-1.5 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6 text-center"
-                                />
+                            />
                         </div>
                     </div>
 
@@ -100,13 +100,14 @@ const Login = () => {
                         </button>
                     </div>
                 </form>
-                            <div className="text-sm">
-                                <Link className="font-semibold text-indigo-600 hover:text-indigo-500" to={'/forget-pass'}>
-                                    Forget Password
-                                </Link>
-                            </div>
+
+
 
                 <p className="mt-10 text-center text-sm text-gray-500">
+                    <Link className="font-semibold text-indigo-600 hover:text-indigo-500 text-center" to={'/forget-pass'}>
+                        Forget Password
+                    </Link>
+
                     Not a member?
                     <Link style={{ textAlign: 'center', display: 'block', marginTop: '5px' }} to={'/signup'}>
                         SIGN UP
