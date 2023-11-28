ab7ce44995dfc215ab138588f9e415450f179047
Vimal-45 <dvimal45@gmail.com>
2023-11-28 06:13:49 +0530

    add loading

--- src/Componets/Login.jsx
+++ src/Componets/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
     const myRef = useRef();
 
     useEffect(() => {
@@ -16,11 +17,13 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email, password);
+        setLoading(true)
+        // console.log(email, password);
         axios.post('https://project-name-backend-4jrk.onrender.com/api/user/login', { email: email, password: password })
             .then((res) => {
                 if (!res.data.email) {
                     alert(res.data.message);
+                    setLoading(false)
                 } else {
                     const token = res.data.token;
                     localStorage.setItem('EMAIL', email);
@@ -37,6 +40,8 @@ const Login = () => {
     };
 
     return (
+
+
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img style={{ height: 120 }}
@@ -95,8 +100,27 @@ const Login = () => {
                         <button
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Sign in
+                            disabled={loading}                        
+                            >
+                                    {loading ? (
+                                <div aria-label="Loading..." role="status" className="flex items-center space-x-2">
+                                    <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
+                                    <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
+                                    </svg>
+
+                                    <span className="text-4xl font-medium text-gray-500">Loading...</span>
+                                </div>
+                            ) : (
+                                'Sign in'
+                            )}
+                        
                         </button>
                     </div>
                 </form>
@@ -115,6 +139,7 @@ const Login = () => {
                 </p>
             </div>
         </div>
+   
     );
 };
 
